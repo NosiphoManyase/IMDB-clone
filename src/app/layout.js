210ab9b99877ce3586f7df352b6a7d2594f0fc2b
Nosipho <manyasenosipho@gmail.com>
2023-03-03 11:54:37 +0200

    create header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,16 @@
+import Header from '@/components/Header'
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
-      <body>{children}</body>
+      <body>
+        <Header />
+        {/* Navbar */}
+        {/* Searchboz */}
+
+        {children}
+      </body>
     </html>
   )
 }
