fe642760e5115e426b129ea8574189e9f3058675
Nosipho <manyasenosipho@gmail.com>
2023-03-08 09:03:22 +0200

    deploy fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import Header from '@/components/Header'
 import Navbar from '@/components/Navbar'
 import SearchBox from '@/components/SearchBox'
 import './globals.css'
-import Providers from './providers'
+import Providers from './Providers'
 
 export default function RootLayout({ children }) {
   return (
